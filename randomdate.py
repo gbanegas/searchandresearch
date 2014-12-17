05edcff4d774926f109f3fafb195d9d7b301f28c
Vader@sith <gustavosouzabanegas@gmail.com>
2014-12-16 23:36:38 -0200

    adicionado arquivos responsaveis por efetuar o serve.py

--- randomdate.py
+++ randomdate.py
@@ -12,8 +12,5 @@ class RandomDate(object):
     def random_date(self, start, end, prop):
         return self.str_time_prop(start, end, '%d/%b/%Y:%H:%M:%S', prop)
 
-
-
-
-ra = RandomDate()
-print ra.random_date("1/Aug/2008:1:30:00", "1/Nov/2014:4:50:00", random.random())
+    def get_random(self):
+        return self.random_date("1/Aug/2008:1:30:00", "1/Nov/2014:4:50:00", random.random())
--- server.py
+++ server.py
@@ -8,13 +8,15 @@ import threading
 import time
 import os
 from random import randint
+from randomdate import RandomDate
 
 class Server(threading.Thread):
     USER_IDS = ['5352b590-05ac-11e3-9923-c3e7d8408f3a', 'f85f124a-05cd-11e3-8a11-a8206608c529', '5352b590-05cd-11e3-8a11-9923-c3e7d84005cd']
-    INPUT = ['177.126.180.83 - - [15/Aug/2013:13:54:38 -0300] \"GET /meme.jpg HTTP/1.1\" 200 2148 \"-\"\"userid=','177.126.180.83 - - [15/Aug/2013:13:54:38 -0300] \"GET /teste.jpg HTTP/1.1\" 200 2148 \"-\"\"userid=','127.0.0.1 - - [15/Aug/2013:13:54:38 -0300] \"GET /meme.jpg HTTP/1.1\" 200 2148 \"-\"\"userid=']
+    INPUT = ['177.126.180.83 - - ',,'127.0.0.1 - - ']
     identificador = 0
     file_to_write = None
     filename = None
+    r_date = RandomDate()
     def __init__(self, id):
         threading.Thread.__init__(self)
         self.identificador = id
@@ -30,7 +32,7 @@ class Server(threading.Thread):
             user_index = randint(0,2)
             input_index = randint(0,2)
             self.file_to_write= open(self.filename, 'w')
-            log_input = self.INPUT[input_index] + self.USER_IDS[user_index] + '\"' + '\n'
+            log_input = self.INPUT[input_index] + r_date.get_random() + ' \"GET /meme.jpg HTTP/1.1\" 200 2148 \"-\"\"userid=' + self.USER_IDS[user_index] + '\"' + '\n'
             self.file_to_write.write(log_input)
             self.file_to_write.close()
             print log_input
