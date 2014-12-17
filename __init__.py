9dae201707164f7bfe122b7435fbbe58177e45cd
Vader@sith <gustavosouzabanegas@gmail.com>
2014-12-17 01:41:07 -0200

    add unit test

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    Gustavo Banegas
+    ~~~~~~~~
+
+    :copyright: (c) 2014 by the Gustavo Banega.
+    :license: BSD
+    """
+__version__ = '0.10-dev
--- unittest/test_parser.py
+++ unittest/test_parser.py
@@ -6,20 +6,26 @@ Created on 15 Dec 2014
 
 import random
 import unittest
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
 from parse import Parse
 
 class TestParser(unittest.TestCase):
 
     def setUp(self):
-        self.parse = Parse()
+        self.parse = Parse('entrada.txt')
 
     def test_parse(self):
-        random.shuffle(self.seq)
-        self.seq.sort()
-        self.assertEqual(self.seq, range(10))
-
-        # should raise an exception for an immutable sequence
-        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+        self.parse.parse_file()
+        path = os.getcwd() + '/server_1/5352b590-05ac-11e3-9923-c3e7d8408f3a'
+        self.assertEqual(os.path.isfile(path), True)
+        f = open(path,'r')
+        line = f.readline()
+        self.assertEqual(line, '177.126.180.83 - - [15/Aug/2013:13:54:38 -0300] \"GET /meme.jpg HTTP/1.1\" 200 2148 \"-\" \"userid=5352b590-05ac-11e3-9923-c3e7d8408f3a\"\n')
+        
 
 if __name__ == '__main__':
     unittest.main()
